211bb4fb1699bcbb5ed2943d4ce5bdd92ed5f086
ig-tangka <ignatiustangka@gmail.com>
2023-03-30 09:35:13 +0800

    third commit

---
+++ src/main/kotlin/com/igtangka/kotlin/cp3/App1.kt
@@ -0,0 +1,41 @@
+package com.igtangka.kotlin.cp3
+
+import kotlin.math.abs
+
+fun compareModule(num1: Int, num2: Int, mod: Int): Int {
+    val mod1 = if (num1 < 0) abs(num1) % mod else num1 % mod
+    val mod2 = if (num2 < 0) abs(num2) % mod else num2 % mod
+    if (mod1 < mod2) {
+        return -1
+    } else if (mod1 > mod2) {
+        return 1
+    } else {
+        if (num1 % 2 == 0 && num2 % 2 == 1) {
+            return 1
+        } else if (num1 % 2 == 1 && num2 % 2 == 0) {
+            return -1
+        } else if (num1 % 2 == 1 && num2 % 2 == 1) {
+            return if (num1 < num2) 1 else -1
+        } else {
+            return if (num1 < num2) -1 else 1
+        }
+    }
+}
+
+fun main() {
+    while (true) {
+        val (n, m) = readLine()!!.split(" ").map { it.toInt() }
+        if (n == 0 && m == 0) break
+        val numbers = mutableListOf<Int>()
+        repeat(n) {
+            numbers.add(readLine()!!.toInt())
+        }
+        numbers.sortWith(Comparator { num1, num2 ->
+            compareModule(num1, num2, m)
+        })
+        val x = readLine()
+        println("$n $m")
+        numbers.forEach { println(it) }
+        println("0 0")
+    }
+}
---
+++ src/main/kotlin/com/igtangka/kotlin/cp3/App2.kt
@@ -0,0 +1,35 @@
+package com.igtangka.kotlin.cp3
+
+import java.util.*
+
+fun main(args: Array<String>) {
+    val scanner = Scanner(System.`in`)
+    val t = scanner.nextInt()
+
+    for (i in 1..t) {
+        val n = scanner.nextInt()
+        val matrix = Array(n) { IntArray(n) }
+
+        for (j in 0 until n) {
+            for (k in 0 until n) {
+                matrix[j][k] = scanner.nextInt()
+            }
+        }
+
+        var symmetric = true
+
+        for (j in 0 until n) {
+            for (k in 0 until n) {
+                if (matrix[j][k] != matrix[n - j - 1][n - k - 1] || matrix[j][k] < 0) {
+                    symmetric = false
+                    break
+                }
+            }
+            if (!symmetric) break
+        }
+
+        val result = if (symmetric) "Symmetric." else "Non-symmetric."
+        println("Test #$i: $result")
+    }
+
+}
